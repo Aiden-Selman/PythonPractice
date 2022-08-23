19eebc0eaa1365e418034d1f552c3c4d4155cf5a
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-23 09:32:55 -0500

    Update PythonPractice.py
    Added an example of some simple python syntax

--- PythonPractice.py
+++ PythonPractice.py
@@ -1,2 +1,13 @@
 # A simple print statement
 print("Hello, World!")
+
+# python indentation is very important. In other languages its for readability, in python its crucitial to execution
+# python uses indentation to indicate a block of code
+if 5 > 2:
+  print("Five is greater than two!")
+
+# this will work
+if 5 > 2:
+print("Five is greater than two!")
+
+# this gives a syntax error
