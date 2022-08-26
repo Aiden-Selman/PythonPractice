e548bb42d6cd094bcdfbdb34977740b104ea0317
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-25 21:24:15 -0500

    Update PythonPractice.py
    Added an example of casting variables

--- PythonPractice.py
+++ PythonPractice.py
@@ -15,3 +15,8 @@ print("Five is greater than two!")
 # how to declare variables in python
 x = 5
 y = "Hello, World!"
+
+# casting variables
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
