e1e5c9bd4651ee216fa622985cfb5b5d9303af01
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-26 22:19:17 -0500

    Update PythonPractice.py
    Added an example of how to get the type of a variable

--- PythonPractice.py
+++ PythonPractice.py
@@ -20,3 +20,9 @@ y = "Hello, World!"
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
+
+# how to get the type of a variable 
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
