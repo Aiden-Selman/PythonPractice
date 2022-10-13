895cfe1d75ac9f04ff437831b57044f56ae653e6
Ectectraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-10-12 22:16:24 -0500

    Update PythonPractice.py
    Added an example of a global variable

--- PythonPractice.py
+++ PythonPractice.py
@@ -49,3 +49,13 @@ print(z)
 # The Python print() function is often used to output variables.
 x = "Python is awesome"
 print(x)
+
+# Global Variables
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
