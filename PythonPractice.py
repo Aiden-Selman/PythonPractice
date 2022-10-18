abb8c499ea57359509400073c8852ac395daf05c
Ectectraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-10-17 22:10:47 -0500

    Update PythonPractice.py
    Added multiple examples of using the print method

--- PythonPractice.py
+++ PythonPractice.py
@@ -70,3 +70,20 @@ def myfunc():
 myfunc()
 
 print("Python is " + x)
+
+# Output Variables
+# The Python print() function is often used to output variables.
+x = "Python is awesome"
+print(x)
+
+# In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
